af8c42433942da7bfb99a572ee99d3d2a2152873
Jacob <jdandreas@gmail.com>
2017-01-22 17:11:17 -0800

    first

---
+++ main.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python2
+
+from models import *
+
+from collections import namedtuple
+import tensorflow as tf
+
+LABELS = {
+    "-0.667": 0,
+    "-0.333": 1,
+    "0.000": 2,
+    "0.333": 3,
+    "0.667": 4
+}
+
+N_BATCH = 100
+N_CONST_FEATURES = 4
+N_SUMMARY_FEATURES = 13
+
+N_TRAIN = 50000
+N_VAL = 1000
+
+N_UPDATE = 100
+
+Datum = namedtuple("Datum", ["label", "summary_features", "const_features"])
+
+def read_data():
+    data = []
+    max_constituents = 0
+    with open("data/data.txt") as data_f:
+        c = 0
+        for line in data_f:
+            c += 1
+            if c > 51000:
+                break
+            parts = line.strip().split()
+            #print line
+            charge = parts[4]
+            label = LABELS[charge]
+            bulk_features = np.asarray([float(f) for f in parts[5:6]])
+            const_parts = parts[6:]
+            assert len(const_parts) % 4 == 0
+            const_features = []
+            for i_const in range(0, len(const_parts), 4):
+                const = const_parts[i_const:i_const+4]
+                const_features.append([float(f) for f in const])
+            const_features = np.asarray(const_features)
+
+            const_mins = np.min(const_features, axis=1)
+            const_maxes = np.max(const_features, axis=1)
+            const_means = np.mean(const_features, axis=1)
+            summary_features = np.concatenate(
+                    (bulk_features, const_mins, const_maxes, const_means))
+
+            data.append(Datum(label, bulk_features, const_features))
+            max_constituents = max(max_constituents, const_features.shape[0])
+            n_summary_features = len(summary_features)
+
+    return data, max_constituents
+
+def do_train_step(model, loader, train_data, session):
+    loss, _ = session.run([model.t_loss, model.o_train], loader.load(train_data))
+    return loss
+
+def do_val_step(model, loader, train_data, val_data, session):
+    train_acc, = session.run([model.t_acc], loader.load(train_data[:N_BATCH], sample=False))
+    val_acc = 0
+    for i_batch in range(0, N_VAL, N_BATCH):
+        val_batch = val_data[i_batch : i_batch + N_BATCH]
+        va, = session.run([model.t_acc], loader.load(val_batch, sample=False))
+        val_acc += va
+    return [train_acc, val_acc / (N_VAL / N_BATCH)]
+
+def main():
+    data, max_constituents = read_data()
+    train_data = data[:N_TRAIN]
+    val_data = data[N_TRAIN:N_TRAIN+N_VAL]
+
+    loader = DataLoader(
+            N_BATCH, N_SUMMARY_FEATURES, N_CONST_FEATURES, max_constituents)
+    #model = MlpModel((len(LABELS),), loader)
+    #model = MlpModel((256, 256, len(LABELS),), loader)
+    model = RnnModel(256, len(LABELS), loader)
+
+    session = tf.Session()
+    session.run(tf.initialize_all_variables())
+
+    loss = 0
+    for t in range(100000):
+        loss += do_train_step(model, loader, train_data, session)
+        if t > 0 and t % N_UPDATE == 0:
+            loss /= N_UPDATE
+            acc = do_val_step(model, loader, train_data, val_data, session)
+            print loss, acc
+            loss = 0
+
+if __name__ == "__main__":
+    main()
---
+++ models.py
@@ -0,0 +1,75 @@
+import net
+
+import numpy as np
+import tensorflow as tf
+
+class DataLoader(object):
+    def __init__(
+            self, n_batch, n_summary_features, n_const_features,
+            max_constituents):
+        self.n_batch = n_batch
+        self.t_label = tf.placeholder(tf.int32, (n_batch,))
+        self.t_summary_features = tf.placeholder(
+                tf.float32, (n_batch, n_summary_features))
+        self.t_const_features = tf.placeholder(
+                tf.float32, (n_batch, max_constituents, n_const_features))
+        self.t_const_len = tf.placeholder(tf.int32, (n_batch))
+        self.random = np.random.RandomState(0)
+
+    def load(self, data, sample=True):
+        label = np.zeros(self.t_label.get_shape(), dtype=np.int32)
+        summary_features = np.zeros(self.t_summary_features.get_shape())
+        const_features = np.zeros(self.t_const_features.get_shape())
+        const_len = np.zeros(self.t_const_len.get_shape(), dtype=np.int32)
+
+        if sample:
+            indices = self.random.randint(len(data), size=self.n_batch)
+        else:
+            assert len(data) == self.n_batch
+            indices = list(range(self.n_batch))
+        for i, i_d in enumerate(indices):
+            datum = data[i_d]
+            label[i] = datum.label
+            summary_features[i, :] = datum.summary_features
+            l = datum.const_features.shape[0]
+            const_len[i] = l
+            const_features[i, :l, :] = datum.const_features
+        
+        return {
+            self.t_label: label,
+            self.t_summary_features: summary_features,
+            self.t_const_features: const_features,
+            self.t_const_len: const_len
+        }
+
+class MlpModel(object):
+    def __init__(self, layers, loader):
+        self.t_scores, v_model = net.mlp(loader.t_summary_features, layers)
+        self.t_loss = tf.reduce_mean(
+                tf.nn.sparse_softmax_cross_entropy_with_logits(
+                    self.t_scores, loader.t_label))
+        self.t_acc = tf.reduce_mean(tf.cast(
+                tf.nn.in_top_k(self.t_scores, loader.t_label, 1), tf.float32))
+
+        optimizer = tf.train.AdamOptimizer(0.0003)
+        self.o_train = optimizer.minimize(self.t_loss, var_list=v_model)
+
+class RnnModel(object):
+    def __init__(self, n_hidden, n_labels, loader):
+        cell = tf.nn.rnn_cell.GRUCell(n_hidden)
+        _, t_state = tf.nn.dynamic_rnn(
+                cell, loader.t_const_features, loader.t_const_len,
+                dtype=tf.float32)
+        t_inputs = tf.concat(1, (loader.t_summary_features, t_state))
+        self.t_scores, _ = net.mlp(t_inputs, (n_labels,))
+
+        v_model = tf.get_collection(tf.GraphKeys.VARIABLES)
+
+        self.t_loss = tf.reduce_mean(
+                tf.nn.sparse_softmax_cross_entropy_with_logits(
+                    self.t_scores, loader.t_label))
+        self.t_acc = tf.reduce_mean(tf.cast(
+                tf.nn.in_top_k(self.t_scores, loader.t_label, 1), tf.float32))
+
+        optimizer = tf.train.AdamOptimizer(0.003)
+        self.o_train = optimizer.minimize(self.t_loss, var_list=v_model)
